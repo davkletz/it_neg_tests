92d5a261ae3460ab93ed287333db73f71c3bf865
davkletz <david.kletz@sorbonne-nouvelle.fr>
2023-07-06 13:10:33 +0200

    Update two_sentences.py

--- 2nd_exp/two_sentences.py
+++ 2nd_exp/two_sentences.py
@@ -18,7 +18,7 @@ print(f"Number of texts from a site containing 'wiki' in their URL: {len(paisa_w
 i = 0
 print(len(paisa_wiki))
 shuffle(paisa_wiki)
-#paisa_wiki = paisa_wiki[:100]
+paisa_wiki = paisa_wiki[:5000]
 
 sent = []
 
@@ -29,9 +29,42 @@ for text in paisa_wiki:
     tokens = nltk.sent_tokenize(text)
 
     for element in range(len(tokens)-1):
-        sent.append(tokens[element] + " " + tokens[element+1])
+        new_sent = tokens[element] + " " + tokens[element+1]
+
+        if i<=5:
+            print(new_sent)
+        if len(new_sent) > 25:
+            sent.append(new_sent)
 
 print(f"Number of sentences: {len(sent)}")
+
+
+
+
+sent_CpTn = []
+sent_CnTp = []
+
+Cn_patt = r"\b[Nn]on\b.*\..*\."  # finds the negation in a sentence
+Tn_patt = r"\..*\b[Nn]on\b.*\."  # finds the negation in a sentence
+
+
+
+for s in sent:
+  matches1 = re.search(Cn_patt, s)
+  if matches1:
+    sent_CnTp.append(s)
+  else:
+    matches2 = re.search(Tn_patt, s)
+    if matches2:
+       sent_CpTn.append(s)
+
+for elem in sent_CnTp:
+  double_neg = re.search(Tn_patt, elem)
+  if double_neg:
+    sent_CnTp.remove(elem)
+
+
+
 
 
 
