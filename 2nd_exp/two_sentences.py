1871315ff255792f77a22e76ccd162b1ee2b6dc2
davkletz <david.kletz@sorbonne-nouvelle.fr>
2023-07-06 13:43:59 +0200

    Update two_sentences.py

--- 2nd_exp/two_sentences.py
+++ 2nd_exp/two_sentences.py
@@ -1,9 +1,7 @@
 import re
 from random import shuffle, seed
 import nltk
-
 seed(42)
-
 
 with open(r"../data/paisa.raw.utf8", encoding='utf8') as infile:
   paisa = infile.read()
@@ -22,10 +20,25 @@ print(len(paisa_wiki))
 shuffle(paisa_wiki)
 paisa_wiki = paisa_wiki[:5000]
 
-
-nb_sents_to_catch = 5000
-
 sent = []
+
+
+for text in paisa_wiki:
+    if i%100 == 0:
+        print(f"Text number {i}, {len(sent)} sentences found")
+    i += 1
+    tokens = nltk.sent_tokenize(text)
+
+    for element in range(len(tokens)-1):
+        new_sent = tokens[element] + " " + tokens[element+1]
+
+        if i<=5:
+            print(new_sent)
+        if len(new_sent) > 25 and not "\n" in new_sent:
+            sent.append(new_sent)
+
+print(f"Number of sentences: {len(sent)}")
+
 
 
 
@@ -38,48 +51,61 @@ CpTp = []
 CpTn = []
 
 
+for s in sent:
+  matches1 = re.search(Cn_patt, s)
+  if matches1:
+    Cn.append(s)
+  else:
+    matches2 = re.search(Tn_patt, s)
+    if matches2:
+       CpTn.append(s)
+    else:
+        CpTp.append(s)
+
+
 CnTp = []
 CnTn = []
-
-
-for text in paisa_wiki:
-
-    if len(CnTn) >= nb_sents_to_catch and len(CnTp) >= nb_sents_to_catch and len(CpTn) >= nb_sents_to_catch and len(CpTp) >= nb_sents_to_catch:
-        break
-
-
-    if i%100 == 0:
-        print(f"Text number {i}, {len(sent)} sentences found")
-    i += 1
-    tokens = nltk.sent_tokenize(text)
-
-    for element in range(len(tokens)-1):
-        new_sent = tokens[element] + " " + tokens[element+1]
-
-
-        if len(new_sent) > 25 and not "\n" in new_sent:
-
-
-            matches1 = re.search(Cn_patt, new_sent)
-            if matches1:
-                double_neg = re.search(Tn_patt, new_sent)
-                if double_neg:
-                    CnTn.append(new_sent)
-                else:
-                    CnTp.append(new_sent)
-            else:
-                matches2 = re.search(Tn_patt, new_sent)
-                if matches2:
-                    CpTn.append(new_sent)
-                else:
-                    CpTp.append(new_sent)
-
-print(f"Number of sentences: {len(sent)}")
-
+for elem in Cn:
+    double_neg = re.search(Tn_patt, elem)
+    if double_neg:
+        CnTn.append(elem)
+    else:
+        CnTp.append(elem)
 
 
 print(f"Number of sent_CnTp sentences : {len(CnTp)} \n\nNumber of CpTn sentences : {len(CpTn)}\n\nNumber of CnTp sentences : {len(CnTp)}\n\nNumber of CnTn sentences : {len(CnTn)}\n\n")
 
+
+shuffle(CnTp)
+
+
+print("\n\n######\n\n")
+for k in range(10):
+    print("\n")
+    print(CnTp[k])
+
+shuffle(CpTn)
+
+
+print("\n\n######\n\n")
+
+for k in range(10):
+    print("\n")
+    print(CpTn[k])
+
+shuffle(CnTn)
+
+print("\n\n######\n\n")
+for k in range(10):
+    print("\n")
+    print(CnTn[k])
+
+shuffle(CpTp)
+
+print("\n\n######\n\n")
+for k in range(10):
+    print("\n")
+    print(CpTp[k])
 
 
 
