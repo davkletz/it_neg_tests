0a7e0617d128c854deeb7534dd85e9cca64c4c84
davkletz <david.kletz@sorbonne-nouvelle.fr>
2023-07-06 13:05:08 +0200

    Update two_sentences.py

--- 2nd_exp/two_sentences.py
+++ 2nd_exp/two_sentences.py
@@ -14,19 +14,23 @@ paisa_wiki = re.findall(wiki_pattern, paisa)
 
 print(f"Number of texts from a site containing 'wiki' in their URL: {len(paisa_wiki)}")
 
-sent = []
-
-pattern = r"(?<= )[A-Z][a-z ]*[,:]?[a-z ]+[,:]?[a-z ][,:]?[a-z]+\. \b[A-Z][a-z ]*[,:]?[a-z ]+[,:]?[a-z ][,:]?[a-z]+\. \b"  # finds series of two sentences
-
 
 i = 0
 print(len(paisa_wiki))
 shuffle(paisa_wiki)
 #paisa_wiki = paisa_wiki[:100]
 
+sent = []
+
 
 for text in paisa_wiki:
     tokens = nltk.sent_tokenize(text)
     print(len(tokens))
+
+    for element in range(len(tokens)-1):
+        sent.append(tokens[element] + " " + tokens[element+1])
+
+print(f"Number of sentences: {len(sent)}")
+
 
 
